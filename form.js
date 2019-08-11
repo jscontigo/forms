ec83f1c5105ad1b05ca5734da30af59fd75cf801
Tejas Rodriquez <jscontigo@gmail.com>
2019-08-11 16:15:08 -0700

    actualizado forms.js de nuevo

--- form.js
+++ form.js
@@ -4,7 +4,7 @@ f.onsubmit = handleChange;
 function handleChange(e){
     //prevent submitting
     e.preventDefault();
-    console.log(`mah name is ${e.target.childNodes[1].value}`);
+    console.log(`me llamo ${e.target.childNodes[1].value}`);
     let hola = document.querySelector('#hola');
     hola.innerHTML = '';
     let h1 = document.createElement('h1');
